5e036e47a4b2e97118bb56f89f20468edc5af784
giovanix50 <tristan.dominguez.2004@gmail.com>
2026-04-17 16:04:16 -0600

    x

---
+++ Controllers/estatus_warehouse/status_warehouse_controller.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+
+[ApiController]
+public class EstatusWarehouseController : ControllerBase
+{
+    private readonly NpgsqlDataSource pool;
+
+    public EstatusWarehouseController(NpgsqlDataSource pool)
+    {
+        this.pool = pool;
+    }
+
+    [HttpGet("estatus_warehouse")]
+    public IActionResult ObtenerEstatusWarehouse(string buscar = "")
+    {
+        try
+        {
+            using var conn = pool.OpenConnection();
+            using var cmd = new NpgsqlCommand(@"
+                SELECT
+                    id,
+                    estatus_id,
+                    descripcion
+                FROM estatus_warehouse
+                WHERE
+                    @buscar = ''
+                    OR descripcion ILIKE @texto
+                    OR CAST(estatus_id AS TEXT) ILIKE @texto
+                ORDER BY estatus_id
+            ", conn);
+
+            cmd.Parameters.AddWithValue("@buscar", buscar ?? "");
+            cmd.Parameters.AddWithValue("@texto", "%" + (buscar ?? "") + "%");
+
+            using var r = cmd.ExecuteReader();
+
+            var lista = new List<object>();
+
+            while (r.Read())
+            {
+                lista.Add(new
+                {
+                    id = r.GetInt32(0),
+                    estatus_id = r.GetInt32(1),
+                    descripcion = r.IsDBNull(2) ? "" : r.GetString(2)
+                });
+            }
+
+            return Ok(lista);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
+}
