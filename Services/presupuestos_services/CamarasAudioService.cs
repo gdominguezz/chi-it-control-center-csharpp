4e17fd9eb16aa841cc59f7f14401719d3dfbdd99
giovanix50 <tristan.dominguez.2004@gmail.com>
2026-05-06 19:38:40 -0600

    camaras de audio con soft delete

--- Services/presupuestos_services/CamarasAudioService.cs
+++ Services/presupuestos_services/CamarasAudioService.cs
@@ -346,6 +346,7 @@ public class CamarasAudioService
     private static (string where, List<(string key, object? val)> parms) ConstruirWhere(CamaraAudioFiltros f)
     {
         var conds = new List<string>();
+        conds.Add("(activo IS NULL OR activo = true)");
         var parms = new List<(string, object?)>();
         var idx = 1;
 
